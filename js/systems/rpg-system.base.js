669431e70558c1f3e905035da8289f710861d82b
CouhDndDeo <postupde@gmail.com>
2026-03-11 23:50:23 +0300

    Update rpg-system.base.js

--- js/systems/rpg-system.base.js
+++ js/systems/rpg-system.base.js
@@ -1,21 +1,49 @@
-// Абстракция для любой игровой системы
+/**
+ * Базовый абстрактный класс для игровой системы
+ */
 export class RPGSystem {
-  constructor(id, name, config) {
+  constructor(id, name, config = {}) {
+    if (!id || !name) {
+      throw new Error('RPGSystem requires id and name');
+    }
     this.id = id;
     this.name = name;
     this.config = config;
   }
 
-  // Переопределяется в наследниках
+  /**
+   * Бросок кубов по формуле
+   * @param {string} formula 
+   * @param {Object} context 
+   * @returns {RollResult|Object}
+   */
   rollDice(formula, context = {}) {
-    throw new Error('rollDice() must be implemented');
+    throw new Error('rollDice() must be implemented by child class');
   }
 
+  /**
+   * Расчёт модификатора характеристики
+   * @param {number} statValue 
+   * @returns {number}
+   */
   calculateModifier(statValue) {
     throw new Error('calculateModifier() must be implemented');
   }
 
+  /**
+   * Шаблон персонажа для этой системы
+   * @returns {Object}
+   */
   getCharacterTemplate() {
     return {};
+  }
+
+  /**
+   * Валидация данных персонажа
+   * @param {Object} character 
+   * @returns {{valid: boolean, errors: string[]}}
+   */
+  validateCharacter(character) {
+    return { valid: true, errors: [] };
   }
 }
