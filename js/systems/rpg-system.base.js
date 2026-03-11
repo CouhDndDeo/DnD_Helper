5f23695961a52ff957d36c051e58f345c881fd13
CouhDndDeo <postupde@gmail.com>
2026-03-11 23:16:32 +0300

    Update rpg-system.base.js

--- js/systems/rpg-system.base.js
+++ js/systems/rpg-system.base.js
@@ -1,1 +1,21 @@
+// Абстракция для любой игровой системы
+export class RPGSystem {
+  constructor(id, name, config) {
+    this.id = id;
+    this.name = name;
+    this.config = config;
+  }
 
+  // Переопределяется в наследниках
+  rollDice(formula, context = {}) {
+    throw new Error('rollDice() must be implemented');
+  }
+
+  calculateModifier(statValue) {
+    throw new Error('calculateModifier() must be implemented');
+  }
+
+  getCharacterTemplate() {
+    return {};
+  }
+}
